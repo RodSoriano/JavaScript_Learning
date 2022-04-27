53b1906aa45e1302f8f81b3882e8af1a9f91a113
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-27 11:34:47 -0600

    Hello World

---
+++ Simple Excercises/ImprovedHello.js
@@ -0,0 +1,11 @@
+// Write a program that asks the user for his first name and last name. 
+// It then concatenates them and displays them in a single sentence.
+
+//console.log("Hello World");
+
+const fisrtName = prompt("Enter Your First Name:");
+const lastName = prompt("Enter Your Last Name:");
+
+let hello = `Hello, ${fisrtName} ${lastName}`;
+
+console.log(hello);
