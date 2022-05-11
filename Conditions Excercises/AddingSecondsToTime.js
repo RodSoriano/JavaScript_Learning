4eda0993ba5d06dbb56d24cc905cffa03f5926b5
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-11 10:49:41 -0600

    conditional excercise

--- Conditions Excercises/AddingSecondsToTime.js
+++ Conditions Excercises/AddingSecondsToTime.js
@@ -1,7 +1,7 @@
 //Write a program that asks for a time under the form of three information (hours, minutes, seconds).
 //The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
 
-//still working
+//not sure about this solution
 
 const hours     = Number(prompt("Enter the hours:"));
 const minutes   = Number(prompt("Enter the minutes:"));
@@ -9,19 +9,69 @@ const seconds   = Number(prompt("Enter the seconds:"));
 
 if(Number.isInteger(hours) && Number.isInteger(minutes) && Number.isInteger(seconds))
     {
-        if(seconds > 60)
+        seconds ++;
+
+        if(seconds >= 60)
             {
                 minutes++;
                 seconds = 0;
+
+                console.log(`In a second, time will be ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
+
+                if (minutes >= 60)
+                    {
+                        hours++;
+                        minutes = 0;
+                        seconds = 0;
+
+                        console.log(`In a second, time will be ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
+                    }
             }
-        else if (minutes > 60)
+        else
             {
-                hours++;
-                minutes = 0;
-                seconds = 0;
+                console.log(`In a second, time will be ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
             }
     }
 else
     {
         console.log("Incorrect input, time must be a number.");
+    }
+
+//another solution might be
+
+// We start by testing errors cases
+if (
+        hours >= 0 &&
+        hours <= 23 &&
+        minutes >= 0 &&
+        seconds >= 0 &&
+        seconds <= 59
+   )
+    {
+        seconds++; // Incrementing the seconds
+        
+        if (seconds === 60)
+            {
+                // Next minute: seconds are reset to 0
+                seconds = 0;
+                minutes <= 59 &&
+                minutes++;
+                if (minutes === 60)
+                    {
+                        // Next hour: minutes are reset to 0
+                        minutes = 0;
+                        hours++;
+                        if (hours === 24)
+                            {
+                                // Next second is midnight
+                                hours = 0;
+                            }
+                    }
+            }
+
+        console.log(`In a second, time will be ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
+     }
+else
+    {
+        console.log("Incorrect input!");
     }
